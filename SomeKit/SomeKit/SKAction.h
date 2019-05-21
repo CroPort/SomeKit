043f973a253c33643743e42ec2014ee650b60985
N6112 <chenxiangjing@corp.netease.com>
2019-05-21 10:56:37 +0800

    sayhello

---
+++ SomeKit/SomeKit/SKAction.h
@@ -0,0 +1,13 @@
+//
+//  SKAction.h
+//  SomeKit
+//
+//  Created by Paul Chen on 2019/5/21.
+//  Copyright © 2019 NetEase. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKAction : NSObject
+- (void)sayHello;
+@end
---
+++ SomeKit/SomeKit/SKAction.m
@@ -0,0 +1,16 @@
+//
+//  SKAction.m
+//  SomeKit
+//
+//  Created by Paul Chen on 2019/5/21.
+//  Copyright © 2019 NetEase. All rights reserved.
+//
+
+#import "SKAction.h"
+
+@implementation SKAction
+- (void)sayHello
+{
+    NSLog(@"Hello, world!");
+}
+@end
