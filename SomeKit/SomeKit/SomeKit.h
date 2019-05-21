c1086d566c517a0bef03d6308f7fb29a39235bb3
N6112 <chenxiangjing@corp.netease.com>
2019-05-21 09:46:52 +0800

    init

---
+++ SomeKit/SomeKit/SomeKit.h
@@ -0,0 +1,19 @@
+//
+//  SomeKit.h
+//  SomeKit
+//
+//  Created by Paul Chen on 2019/5/21.
+//  Copyright © 2019 NetEase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SomeKit.
+FOUNDATION_EXPORT double SomeKitVersionNumber;
+
+//! Project version string for SomeKit.
+FOUNDATION_EXPORT const unsigned char SomeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SomeKit/PublicHeader.h>
+
+
